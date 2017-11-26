1aeebacc7f1f60afbed07dc2cc132cad9716888d
Andrew Adler <andrew@Andrews-MacBook-Pro-3.local>
2017-11-26 14:40:32 -0900

    header file

---
+++ cardgames.hpp
@@ -0,0 +1,9 @@
+// cardgames.hpp
+// Andrew Adler and Adrian Antonio
+// 11/26/17
+// This is the header file for the cardgames
+
+#ifndef CARDGAMES_HPP_INCLUDED
+#define CARDGAMES_HPP_INCLUDED
+
+#endif //#ifndef CARDGAMES_HPP_INCLUDED

b272f3e88f927d70aeec74c85d23e6587c9c8f1b
Akantonio <adrianvantonio@yahoo.com>
2017-11-28 06:58:29 -0900

    added things
    I put in a function adding some stuff haven't put them on header

--- blackjack.cpp
+++ blackjack.cpp
@@ -3,13 +3,47 @@
 #include "cardgames.hpp"
 #include "deck.hpp"
 
+//ask the user if they want another card
+bool player1() {
+	cout << "Hit or Stay?" << endl;
+	string playerChoice;
+	getline(cin, playerChoice);
+	if (playerChoice == "hit") {
+		return true;
+	}
+	return false;
+}
+
+void showCards(vector<string> & cards) {
+	cout << "Cards dealt: ";
+	for (auto i : cards) {
+		cout << i << ", ";
+	}
+	cout << endl;
+}
+
 void blackjack() {
     cout << "Running blackjack:" << endl;
     vector<string> cards = Deck::getDeck();
     cout << "Dealing cards..." << endl;
     vector<string> playerCards = Deck::deal(2, cards);
-    cout << "Cards dealt: ";
-    for (auto i : playerCards) {
-        cout << i << ", ";
-    }
+	showCards(playerCards);
+	
+
+	//gives a card
+
+	while (true) {
+
+		if (player1()) {
+			vector<string> plusOneCard = Deck::deal(1, cards);
+			for (auto i : plusOneCard) {
+				playerCards.push_back(i);
+			}
+			showCards(playerCards);
+			cout << endl;
+		}
+		else {
+			break;
+		}
+	}
 }
